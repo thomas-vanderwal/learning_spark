f4cc733926183d24d23b82fab39c9398e58232a8
Thomas Vander Wal <thomasvanderwal@VanderWal-MBP.local>
2018-09-06 16:36:36 -0600

    Better way to store each line of data. instead of a single string it's a list containing two fields

--- src/key_value.py
+++ src/key_value.py
@@ -7,7 +7,7 @@ sc = SparkContext(conf = conf)
 # Read near miss data with DictReader and then iterate through the rows creating key value records.
 with open('../data/near_miss_data.csv') as csv_file:
     asteroids = csv.DictReader(csv_file)
-    key_val= {'{0},{1}'.format(row['neo_reference_id'],row['miss_distance_astronomical']) for row in asteroids}
+    key_val= {(row['neo_reference_id'],float(row['miss_distance_astronomical'])) for row in asteroids}
 
 # Create RDD of key-value pairs
 data = sc.parallelize(key_val)

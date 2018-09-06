ffa469dc846dea963ef4548d08d26bda478e5e02
Thomas Vander Wal <thomasvanderwal@VanderWal-MBP.local>
2018-09-05 22:24:35 -0600

    Formatted data to key-value pair and loaded it into an RDD

--- src/key_value.py
+++ src/key_value.py
@@ -4,9 +4,11 @@ from pyspark import SparkConf, SparkContext
 conf = SparkConf().setMaster("local").setAppName("My App")
 sc = SparkContext(conf = conf)
 
-with open('../data/near_miss_data.csv') as csv_file: 
+# Read near miss data with DictReader and then iterate through the rows creating key value records.
+with open('../data/near_miss_data.csv') as csv_file:
     asteroids = csv.DictReader(csv_file)
+    key_val= {'{0},{1}'.format(row['neo_reference_id'],row['miss_distance_astronomical']) for row in asteroids}
 
-    ast_dict = {row['neo_reference_id']: row['miss_distance_astronomical'] for row in asteroids}
-
-
+# Create RDD of key-value pairs
+data = sc.parallelize(key_val)
+print(data.take(10))

2062a214517196ef2f92a30eac6d86392cb65a17
Thomas Vander Wal <thomasvanderwal@VanderWal-MBP.local>
2018-09-08 17:48:33 -0600

    Added logic for joining two RDDs together

--- src/key_value.py
+++ src/key_value.py
@@ -8,13 +8,15 @@ sc.setLogLevel('OFF')
 # Read near miss data with DictReader and then iterate through the rows creating key value records.
 with open('../data/near_miss_data.csv') as csv_file:
     near_miss_data = csv.DictReader(csv_file)
-    near_miss_kv= {(row['neo_reference_id'],float(row['miss_distance_astronomical'])) for row in near_miss_data}
+    near_miss_kv = {(row['neo_reference_id'], float(row['miss_distance_astronomical']))
+                    for row in near_miss_data}
 
 # Read over asteroid data with DictReader.
 with open('../data/asteroid_data.csv') as csv_file:
     asteroid_data = csv.DictReader(csv_file)
-    asteroid_kv = {(row['neo_reference_id'],(float(row['absolute_magnitude_h']) if len(row['absolute_magnitude_h']) > 0  else 0
-                                            ,float(row['min_orbit_intersection']))) for row in asteroid_data}
+    asteroid_kv = {(row['neo_reference_id'],
+                    (float(row['absolute_magnitude_h']) if row['absolute_magnitude_h'] != '' else 0,
+                     float(row['min_orbit_intersection']))) for row in asteroid_data}
 
 ast_data = sc.parallelize(asteroid_kv).persist()
 
@@ -33,12 +35,21 @@ pha_data = ast_data.filter(lambda x: x[1][0] < 22.0 and  x[1][1] < .05)
 print('First 10 potentially hazardous asteroids: {0}'.format(pha_data.take(10)))
 
 # Using combineByKey to find th per-key average. Here we will use near miss data
-# to find the average miss distance of each asteroid 
+# to find the average miss distance of each asteroid
 
 # Returns a new key-valu RDD with (asteroid, (sum miss distance, count near misses))
-sum_count = nm_data.combineByKey((lambda x: (x, 1))
-                                ,(lambda x, y: (x[0] + y, x[1] + 1))
-                                ,(lambda x, y: (x[0] + y[0], x[1] + y[1])))
+sum_count = nm_data.combineByKey((lambda x: (x, 1)),
+                                 (lambda x, y: (x[0] + y, x[1] + 1)),
+                                 (lambda x, y: (x[0] + y[0], x[1] + y[1])))
 
 results = sum_count.map(lambda x: (x[0], x[1][0]/x[1][1]))
 print('First 10 asteroids and their average miss distance: {0}'.format(results.take(10)))
+
+# Count the times an asteroid has a near miss
+cnt = nm_data.map((lambda x: (x[0], 1))).reduceByKey(lambda x, y: (x+y))
+
+# Join this count RDD to our PHA RDD to find how many times each PHA has came into close
+# Contact with an orbiting body. Results ordered by most near misses
+joined_data = cnt.join(pha_data)
+print('Top 10 PHA asteroids by number of near misses: {}'
+      .format(joined_data.takeOrdered(10, key=lambda x: -x[1][0])))

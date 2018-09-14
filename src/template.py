e5eeff6bf1b66f39bc8591a8c3676fa3c120eaa3
Thomas Vander Wal <thomas.a.vanderwal@gmail.com>
2018-09-13 21:23:23 -0600

    Continued to work through files chapter. Mostly informational and high level, so example file is sparse

--- src/template.py
+++ src/template.py
@@ -1,4 +1,4 @@
 from pyspark import SparkConf, SparkContext
 
 conf = SparkConf().setMaster("local").setAppName("My App")
-sc = SparkContext.(conf = conf)
+sc = SparkContext(conf = conf)
--- src/working_with_files.py
+++ src/working_with_files.py
@@ -1,2 +1,34 @@
 # File for working with examples from Chapter 5
 #  Loading and Saving Your Data
+import json
+import csv
+import io
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setMaster("local").setAppName("My App")
+sc = SparkContext(conf = conf)
+
+#WholeTextFile can be used to load an entire file at once
+#It returns a key-value pair where the key is the name of the text file
+#  - Tip: Spark can read an entire directory with * ie part-*.txt
+wf = sc.wholeTextFiles('../data/asteroid_data.csv')
+
+# Saving text files: RDD.saveAsTextFile(FILE)
+
+#Working with JSON
+input = sc.textFile('../data/testtweet.json')                    
+data = input.map(lambda x: json.loads(x))
+print(type(data))
+
+#output json data to text file
+data.filter(lambda x: x['user']).map(lambda x: json.dumps(x))\
+#            .saveAsTextFile('../data/output/jsondump.txt')
+
+#Reading CSV file. Use a function instead of whole file methon in key_value file
+def load_record(line):
+    """ Parse CSV line """
+    line_buffer = io.StringIO(line)
+    reader = csv.DictReader(line_buffer)
+    return next(reader)
+input = sc.textFile('../data/asteroid_data.csv').map(load_record)
+print('First 5 records of csv reader: {0}'.format(input.take(5)))
